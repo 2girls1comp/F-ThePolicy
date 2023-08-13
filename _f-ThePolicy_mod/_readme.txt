878f14e8c421eff0739660f0bdaff7de52d44b9f
Marco De Mutiis <marcodemutiis@users.noreply.github.com>
2023-08-13 16:15:08 +0200

    .

---
+++ _f-ThePolicy_mod/_readme.txt
@@ -0,0 +1,15 @@
+A mod for GTA V that makes the police read the player's rights (GTA V End User License Agreement) when being wanted by the cops.
+
+GAMEPLAY TRAILER: 
+
+
+INSTALLATION:
+- Copy "FThePolicy.dll" and the "fthepolicy" folder into your scripts folder (if you don't have one, create it) in your GTA V root folder. 
+
+USAGE:
+- the mod is active by default and is triggered when the player is wanted by the police. 
+
+REQUIREMENTS:
+Script Hook V
+Script Hook V Dot Net
+
---
+++ code/F___ThePolicy/F___ThePolicy/F___ThePolicy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+using GTA.Native;
+using GTA;
+using GTA.Math;
+using System.Media;
+
+namespace F___ThePolicy
+{
+    public class F___ThePolicy : Script
+    {
+        SoundPlayer soundPlayer = new SoundPlayer("./scripts/fthepolicy/thepolicy.wav"); //audio player
+        int prevWantedLevel = 0;
+
+        public F___ThePolicy()
+        {
+            Tick += onTick;
+        }
+
+        private void onTick(object sender, EventArgs e)
+        {
+            // when the wanted level switch from 0 to 1 stars
+            if (prevWantedLevel == 0 && Game.Player.WantedLevel > 0)
+            {
+                Wait(4000);
+                soundPlayer.Play(); //start playback of the .wav file
+            }
+            // when the wanted level goes back to zero
+            else if (Game.Player.WantedLevel == 0)
+            {
+                soundPlayer.Stop(); //stop playback 
+            }
+            prevWantedLevel = Game.Player.WantedLevel;
+        }
+    }
+}
